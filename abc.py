32c90ab0e258e5c77d57180959d8922347061fe7
M VAISHNAVI <97328409+VAISHNAVIM1109@users.noreply.github.com>
2022-05-04 21:51:59 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,3 +1,5 @@
+# to connect mysqlworkbench and python
+
 import mysql.connector
 mydb=mysql.connector.connect(user='root',passwd='12345',database="project",autocommit=True,auth_plugin='mysql_native_password',)
 cursor=mydb.cursor(buffered=True)
